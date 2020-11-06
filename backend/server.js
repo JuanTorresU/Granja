5b8e38ec17873a88259b92431f7039b931316dd3
juan <juanotoca@gmail.com>
2020-11-06 16:44:15 -0500

    Visualizacion de las graficas de todos los sensores

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,104 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const API_KEY = "BBFF-b3808660343fa365f90b22577d679c88045"
-var ubidots = require('ubidots');
+const temperaturaId = "5f973b400ff4c3359ef72e26";
+const humedadRelativaId = "5f973b390ff4c3359ef72e25";
+const luminiscenciaId = "5f973b3f0ff4c333e278add9";
+const humedadSuelo3Id = "5f973b3e4763e75f806f0d9a";
+const humedadSuelo2Id = "5f973b3c73efc33e0382dd7f";
+const humedadSuelo1Id = "5f973b3b73efc33daecf8cd1";
+
+const express = require("express");
+const app = express();
+const port = 3000;
+const API_KEY = "BBFF-b3808660343fa365f90b22577d679c88045";
+var ubidots = require("ubidots");
 var client = ubidots.createClient(API_KEY);
-var cors = require('cors')
+var cors = require("cors");
+var temperatura,
+  humedadRelativa,
+  luminiscencia,
+  humedadSuelo1,
+  humedadSuelo2,
+  humedadSuelo3;
 
-app.use(cors())
-    
-app.get('/', (req, res) => {
+app.use(cors());
 
-    client.auth(function () {
-          var v = this.getVariable('5f973b400ff4c3359ef72e26');    
-          v.getValues(function (err, data) {
-            //console.log(data.results);
-            res.send(data.results)
-          });
-        });
-
-})
+app.get("/", (req, res) => {
+  client.auth(function () {
+    f1(res);
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+async function f1(res) {
+  temperatura = await getValuesFromVariable(temperaturaId);
+  humedadRelativa = await getValuesFromVariable(humedadRelativaId);
+  luminiscencia = await getValuesFromVariable(luminiscenciaId);
+  humedadSuelo1 = await getValuesFromVariable(humedadSuelo1Id);
+  humedadSuelo2 = await getValuesFromVariable(humedadSuelo2Id);
+  humedadSuelo3 = await getValuesFromVariable(humedadSuelo3Id);
+  res.send([
+    temperatura,
+    humedadRelativa,
+    luminiscencia,
+    humedadSuelo1,
+    humedadSuelo2,
+    humedadSuelo3,
+  ]);
+}
+function timeConverter(UNIX_timestamp) {
+  var a = new Date(UNIX_timestamp);
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + " " + month + " " + hour + ":" + min + ":" + sec;
+  return time;
+}
+
+function getValuesFromVariable(variableId) {
+  return new Promise(function (resolve, reject) {
+    //Se crea la promesa
+    client.auth(function () {
+      //Metodo del cliente Ubidots, se le pasa una
+      //función anonima con l o que se quiera obtener de la API
+      //Se declara una solicitud de la variable
+      const variable = this.getVariable(variableId);
+      //Este metodo de la variable devuelve los datos con el resolve
+      variable.getValues(function (err, data) {
+        if (err) {
+          reject(err);
+        }
+        resolve(data.results);
+      });
+    });
+  }).then((value) => {
+    //Solamente se cogen los ultimos 10 mdatos y se eliminan los elementos
+    //context y created_at que no son necesarios, ademas se convierte el
+    //timestamp a una fecha legible
+    let values = value.slice(0, 10);
+    values.map((x) => {
+      x.timestamp = timeConverter(x.timestamp);
+      delete x.context;
+      delete x.created_at;
+    });
+    return values;
+  });
+}
--- front/scripts/main.js
+++ front/scripts/main.js
@@ -4,32 +4,126 @@ var settings = {
   timeout: 0,
 };
 
-var timestamp = ["timestamp"];
-var value = ["value"];
+var timeTemp = ["timeTemp"];
+var temperature = ["temperature"];
+var timeHumR = ["timeHumR"];
+var humidity = ["humidity"];
+var timelumi = ["timelumi"];
+var luminiscence = ["luminiscence"];
+var timeSoilM1 = ["timeSoilM1"];
+var soilMoisture1 = ["soilMoisture1"];
+var timeSoilM2 = ["timeSoilM2"];
+var soilMoisture2 = ["soilMoisture2"];
+var timeSoilM3 = ["timeSoilM3"];
+var soilMoisture3 = ["soilMoisture3"];
 
+//Función de ajax que ejecuta el metodo get para obtener los datos que se envían desde
+//el backend
 $.ajax(settings).done(function (response) {
-  response.forEach(function setValues(item) {
-    timestamp.push(item.timestamp);
-    value.push(item.value)
+  //Se hace un mapeo por cada una de las variables para
+  //separar los vectores de tiempo y de valores
+  response[0].forEach(function setValues(item) {
+    timeTemp.push(item.timestamp);
+    temperature.push(item.value);
   });
-  console.log(timestamp);
-  console.log(value);
+  response[1].forEach(function setValues(item) {
+    timeHumR.push(item.timestamp);
+    humidity.push(item.value);
+  });
+  response[2].forEach(function setValues(item) {
+    timelumi.push(item.timestamp);
+    luminiscence.push(item.value);
+  });
+  response[3].forEach(function setValues(item) {
+    timeSoilM1.push(item.timestamp);
+    soilMoisture1.push(item.value);
+  });
+  response[4].forEach(function setValues(item) {
+    timeSoilM2.push(item.timestamp);
+    soilMoisture2.push(item.value);
+  });
+  response[5].forEach(function setValues(item) {
+    timeSoilM3.push(item.timestamp);
+    soilMoisture3.push(item.value);
+  });
 
-  var chart = c3.generate({
-    bindto: "#chart",
+  //Se crea el objeto que genera el gráfico en base a los datos
+  //de cada variable
+  var chart1 = c3.generate({
+    //Identificador de la etiqueta html en index.html
+    //Que va a contener el gráfico de esta variable
+    bindto: "#chart1",
     data: {
-      columns: [
-        //timestamp,
-        value,
-      ],
+      //Se declara el eje x
+      x: "timeTemp",
+      //Se le asiganan los valores de tiempo y magnitud
+      columns: [timeTemp, temperature],
     },
     axis: {
-        x: {
-            type: 'category',
-            timestamp
-        }
-    }
+      x: {
+        type: "category",
+      },
+    },
   });
-  
-});
 
+  var chart2 = c3.generate({
+    bindto: "#chart2",
+    data: {
+      x: "timeHumR",
+      columns: [timeHumR, humidity],
+    },
+    axis: {
+      x: {
+        type: "category",
+      },
+    },
+  });
+  var chart3 = c3.generate({
+    bindto: "#chart3",
+    data: {
+      x: "timelumi",
+      columns: [timelumi, luminiscence],
+    },
+    axis: {
+      x: {
+        type: "category",
+      },
+    },
+  });
+  var chart4 = c3.generate({
+    bindto: "#chart4",
+    data: {
+      x: "timeSoilM1",
+      columns: [timeSoilM1, soilMoisture1],
+    },
+    axis: {
+      x: {
+        type: "category",
+      },
+    },
+  });
+  var chart5 = c3.generate({
+    bindto: "#chart5",
+    data: {
+      x: "timeSoilM2",
+      columns: [timeSoilM2, soilMoisture2],
+    },
+    axis: {
+      x: {
+        type: "category",
+      },
+    },
+  });
+  var chart1 = c3.generate({
+    bindto: "#chart6",
+    data: {
+      x: "timeSoilM3",
+      columns: [timeSoilM3, soilMoisture3],
+    },
+    axis: {
+      x: {
+        type: "category",
+      },
+    },
+  });
+});
